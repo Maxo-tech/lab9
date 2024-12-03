8e2bcbff1c4f8c16982ad3f832fabd356a5ded30
maxo <maxwellonyemaruche@gmail.com>
2024-12-03 21:34:16 +0100

    Final comments
    Added comments in JS for quick recall

--- js/script.js
+++ js/script.js
@@ -1,19 +1,18 @@
 function init(){
 	//add your javascrip between these two lines of code
-	
+	//Here grabbed HTML Elements
 	const entryButton = document.getElementById('entrybutton');
 	const entryInput = document.getElementById('entryinput');
 	const textoutput = document.getElementById('textoutput');
 	
-
-
+//Here Added Event Listener
 entryButton.addEventListener('click', function() {
 	const inputText = entryInput.value;
 
-	
+//Here Added Alert
 	alert('Maxwell Onyemaruche: ' + 'This is a test');
 
-
+	//Here Added changed h2 text
 	textoutput.textContent = 'This is a test';
 });
 
