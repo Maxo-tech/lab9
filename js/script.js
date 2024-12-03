84fc0e7e2a867b20582636d43d7d55f0d6f786a0
maxo <maxwellonyemaruche@gmail.com>
2024-12-03 21:28:55 +0100

    Update JS
    Added event listener, alert and changed h2

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
 function init(){
-//add your javascrip between these two lines of code
- 
+	//add your javascrip between these two lines of code
+	
+	const entryButton = document.getElementById('entrybutton');
+	const entryInput = document.getElementById('entryinput');
+	const textoutput = document.getElementById('textoutput');
+	
 
 
+entryButton.addEventListener('click', function() {
+	const inputText = entryInput.value;
+
+	
+	alert('Maxwell Onyemaruche: ' + 'This is a test');
 
 
+	textoutput.textContent = 'This is a test';
+});
 
-
+}
 
 window.addEventListener('load', init);
